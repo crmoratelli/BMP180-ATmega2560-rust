e74a29966003707f61c7da9844e84904c2762f86
Moratelli <crmoratelli@gmail.com>
2024-03-31 16:16:40 -0300

    Implement panic handler

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,47 @@
 #![no_main]
 use arduino_hal as hal;
 use ufmt_float::uFmt_f32;
-use panic_halt as _;
+use arduino_hal::prelude::*;
+use avr_device;
 
 mod bmp180;
 use bmp180::Bmp180;
 use bmp180::Mode;
+
+#[panic_handler] 
+fn panic(info: &core::panic::PanicInfo) -> ! { 
+    // disable interrupts - firmware has panicked so no ISRs should continue running 
+    avr_device::interrupt::disable(); 
+  
+    // get the peripherals so we can access serial and the LED. 
+    // 
+    // SAFETY: Because main() already has references to the peripherals this is an unsafe 
+    // operation - but because no other code can run after the panic handler was called, 
+    // we know it is okay. 
+    let dp = unsafe { arduino_hal::Peripherals::steal() }; 
+    let pins = arduino_hal::pins!(dp); 
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600); 
+ 
+    // Print out panic location 
+    ufmt::uwriteln!(&mut serial, "Firmware panic!\r").unwrap_infallible(); 
+    if let Some(loc) = info.location() { 
+        ufmt::uwriteln!( 
+            &mut serial, 
+            "  At {}:{}:{}\r", 
+            loc.file(), 
+            loc.line(), 
+            loc.column(), 
+        ) 
+        .unwrap_infallible(); 
+    } 
+  
+    // Blink LED rapidly 
+    let mut led = pins.d13.into_output(); 
+    loop { 
+        led.toggle(); 
+        arduino_hal::delay_ms(100); 
+    } 
+} 
 
 #[arduino_hal::entry]
 fn main() -> ! {
